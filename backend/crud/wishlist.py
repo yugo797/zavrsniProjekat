a260aa55191945beb0da2a2d8aad0dbb9f8beb35
Milica Bjelica <155689648+milicabj@users.noreply.github.com>
2024-07-19 14:44:21 +0200

    modifikacije wishlist i drugih relacija

--- backend/crud/wishlist.py
+++ backend/crud/wishlist.py
@@ -1,41 +1,43 @@
 from sqlalchemy.orm import Session
-from models.wishlist import Wishlist
-from schemas.wishlist import WishlistCreate, WishlistUpdate
-
+from ..models.wishlist import Wishlist, wishlist_movie_association, Movie  
+from ..schemas.wishlist import WishlistCreate
 
 def get_wishlist(db: Session, wishlist_id: int):
     return db.query(Wishlist).filter(Wishlist.id == wishlist_id).first()
 
-
 def get_wishlist_by_user(db: Session, user_id: int):
-    return db.query(Wishlist).filter(Wishlist.user_id == user_id).all()
-
+    return db.query(Wishlist).filter(Wishlist.user_id == user_id).first()
 
 def create_wishlist(db: Session, wishlist: WishlistCreate):
-    db_wishlist = Wishlist(**wishlist.dict())
+    db_wishlist = Wishlist(user_id=wishlist.user_id)
     db.add(db_wishlist)
     db.commit()
     db.refresh(db_wishlist)
+    
+    # Dodavanje filmova u listu želja
+    if wishlist.movie_ids:
+        movies = db.query(Movie).filter(Movie.id.in_(wishlist.movie_ids)).all()
+        db_wishlist.movies.extend(movies)
+        db.commit()
+
     return db_wishlist
 
-
-def update_wishlist(db: Session, wishlist: WishlistUpdate, wishlist_id: int):
+def update_wishlist(db: Session, wishlist_id: int, wishlist: WishlistCreate):
     db_wishlist = db.query(Wishlist).filter(Wishlist.id == wishlist_id).first()
     if db_wishlist:
-        for key, value in wishlist.dict(exclude_unset=True).items():
-            setattr(db_wishlist, key, value)
+        db_wishlist.user_id = wishlist.user_id
+        if wishlist.movie_ids:
+            db_wishlist.movies = db.query(Movie).filter(Movie.id.in_(wishlist.movie_ids)).all()
         db.commit()
         db.refresh(db_wishlist)
     return db_wishlist
 
-
-def delete_wishlist_item(db: Session, wishlist_id: int):
+def delete_wishlist(db: Session, wishlist_id: int):
     db_wishlist = db.query(Wishlist).filter(Wishlist.id == wishlist_id).first()
     if db_wishlist:
         db.delete(db_wishlist)
         db.commit()
     return db_wishlist
-
 
 def get_all_wishlists(db: Session):
     return db.query(Wishlist).all()
