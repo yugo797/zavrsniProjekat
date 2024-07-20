87db762079b33ee3ca1facbec8ec714c0ab1da6e
Yu <milica.m.simovic3@gmail.com>
2024-07-21 00:59:13 +0200

    dodat register html css

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "../styles/loginStyle.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="inputContainer">
+          <input
+            type="email"
+            className="inputField"
+            placeholder="email adresa"
+            required
+          />
+          <input className="inputField" placeholder="korisničko ime" required />
+          <input
+            type="password"
+            className="inputField"
+            placeholder="lozinka"
+            required
+          />
+
+          <button type="button" className="button">
+            Registruj se
+          </button>
+          <span className="otherformlink">
+            <Link to="/register" className="linktoreg">
+              Već imate nalog? <b>Prijavite se ovdje</b>
+            </Link>
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
